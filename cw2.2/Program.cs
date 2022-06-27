e130231e7747e94d22944e2fc288cec33ce185f3
zainabalihu <107780407+zainabalihu@users.noreply.github.com>
2022-06-27 15:36:54 +0300

    cw2.1
    cw2.1

---
+++ cw2.2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cw2._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String heroName = "Jimmy", villainName = "Simon";
+            int heroPower = 1500, villainPower = 1600;
+            
+            Console.WriteLine("The strongest is");
+
+            if (heroPower > villainPower)
+            {
+                Console.WriteLine(" Jimmy ");
+            }
+            
+            else if ( heroPower == villainPower )
+
+            {
+                Console.WriteLine(" Thy have the same power "); 
+            }
+            
+            else 
+            {
+                Console.WriteLine(" simon ");
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
